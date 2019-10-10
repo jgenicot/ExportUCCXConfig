2e53bd8de071333280e20221308f17a176098f11
jeremy.genicot <jeremy@genicot.eu>
2019-10-10 16:33:58 +0200

    sample credential config

---
+++ credentials.sample.py
@@ -0,0 +1,10 @@
+# define hostname + api credentials + custom contacts
+hostname = 'host.name'
+apiusername = 'uccxadmin'
+apipassword = 'uccxpass'
+
+googleusername = 'xxxxxx.apps.googleusercontent.com'
+googlesecret = 'azerty'
+googlerefresh = 'qwerty'
+
+
